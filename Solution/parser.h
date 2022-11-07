0c9bfcc668e29231a091af476807e2e850864b47
Liam Abourousse <liam-alexandre.abourousse@epita.fr>
2022-11-07 21:23:56 +0100

    Created aux files to free space on main

---
+++ Solution/parser.h
@@ -0,0 +1,31 @@
+/* primary_header.h */
+#ifndef PRIMARY_HEADER_H
+#define PRIMARY_HEADER_H
+
+#include <stdio.h>
+
+char **split(char *, int *);
+
+int isNom(char *);
+
+int isVer(char *);
+
+int isAdj(char *);
+
+int isAdv(char *);
+
+char *getType(char *);
+
+char *getLine(char *, int );
+
+char *getNom(char *);
+
+char *getVer(char *);
+
+char *getAdj(char *);
+
+char *getAdv(char *);
+
+int display();
+
+#endif /* PRIMARY_HEADER_H */
